e3e9520b084bbfcd966525251c79200be4d8f8af
Micke Prag <micke.prag@telldus.se>
2020-01-20 13:45:50 +0100

    Parse header extensions in ZipPacket

--- pyzwave/commandclass/Zip.py
+++ pyzwave/commandclass/Zip.py
@@ -15,6 +15,34 @@ from . import ZWaveMessage, registerCmdClass
 registerCmdClass(COMMAND_CLASS_ZIP, "ZIP")
 
 
+class HeaderExtension(dict):
+    default = {}
+
+    def serialize(self, stream: BitStreamWriter):
+        pass
+
+    @classmethod
+    def deserialize(cls, stream: BitStreamReader):
+        retval = {}
+        extLength = stream.byte() - 1
+        val = stream.value(extLength)
+        for optionType, optionValue in cls.tlvIterator(val):
+            pass
+        return retval
+
+    @staticmethod
+    def tlvIterator(pkt):
+        i = 0
+        while i < len(pkt):
+            tlvType = pkt[i]
+            length = pkt[i + 1]
+            tlvValue = pkt[i + 2 : i + 2 + length]
+            i = i + 2 + length
+            yield (tlvType, tlvValue)
+        if i != len(pkt):
+            raise ValueError("BAD TLV")
+
+
 @ZWaveMessage(COMMAND_CLASS_ZIP, COMMAND_ZIP_PACKET)
 class ZipPacket(Message):
     NAME = "ZIP_PACKET"
@@ -37,9 +65,15 @@ class ZipPacket(Message):
         ("seqNo", uint8_t),
         ("sourceEP", uint8_t),  # TODO, actually only 7 bits
         ("destEP", uint8_t),  # TODO, 7 bits + bit address
+        ("headerExtension", HeaderExtension),
         ("command", Message),
     )
 
     ZIP_OPTION_EXPECTED_DELAY = 1
     ZIP_OPTION_MAINTENANCE_GET = 2
     ZIP_OPTION_MAINTENANCE_REPORT = 3
+
+    def parse_headerExtension(self, stream: BitStreamReader):
+        if not self.headerExtIncluded:
+            return {}
+        return HeaderExtension.deserialize(stream)
--- tests/test_cc_zip.py
+++ tests/test_cc_zip.py
@@ -1,8 +1,9 @@
-from pyzwave.commandclass import Zip
+import pytest
+from pyzwave.commandclass import NetworkManagementProxy, Zip
 from pyzwave.message import Message
 
 
-def test_zip_packet():
+def test_zip_packet_networkmanagementproxt_nodelistget():
     pkt = b"#\x02\x80P\x02\x00\x00R\x01\x02"
     msg = Message.decode(pkt)
     assert isinstance(msg, Zip.ZipPacket)
@@ -17,7 +18,39 @@ def test_zip_packet():
     assert msg.moreInformation == False
     assert msg.secureOrigin == True
     assert msg.seqNo == 2
-    assert msg.ackRequest == True
     assert msg.sourceEP == 0
     assert msg.destEP == 0
+    assert type(msg.command) is NetworkManagementProxy.NodeListGet
     assert msg.compose() == pkt
+
+
+def test_zip_packet_networkmanagementproxt_nodelistreport():
+    pkt = b"#\x02\x00\xd0`\x00\x00\x05\x84\x02\x04\x00R\x02\x01\x00\x01!\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+    msg = Message.decode(pkt)
+    assert isinstance(msg, Zip.ZipPacket)
+    assert msg.ackRequest == False
+    assert msg.ackResponse == False
+    assert msg.nackResponse == False
+    assert msg.nackWaiting == False
+    assert msg.nackQueueFull == False
+    assert msg.nackOptionError == False
+    assert msg.headerExtIncluded == True
+    assert msg.zwCmdIncluded == True
+    assert msg.moreInformation == False
+    assert msg.secureOrigin == True
+    assert msg.seqNo == 96
+    assert msg.sourceEP == 0
+    assert msg.destEP == 0
+    assert type(msg.command) is NetworkManagementProxy.NodeListReport
+
+
+def test_zip_tlv():
+    for header, value in Zip.HeaderExtension.tlvIterator(b"\x84\x02\x04\x00"):
+        assert header == 132
+        assert value == b"\x04\x00"
+
+
+def test_zip_tlv_bad_value():
+    with pytest.raises(ValueError):
+        for _ in Zip.HeaderExtension.tlvIterator(b"\x84\x04\x04\x00"):
+            pass
