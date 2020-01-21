5b69b83c2fa7db008450aa3b1892a2bc015e8ce9
Micke Prag <micke.prag@telldus.se>
2020-01-21 14:52:00 +0100

    Add message Basic.Set

--- pyzwave/commandclass/Basic.py
+++ pyzwave/commandclass/Basic.py
@@ -2,6 +2,7 @@ from pyzwave.const.ZW_classcmd import (
     COMMAND_CLASS_BASIC,
     BASIC_GET,
     BASIC_REPORT,
+    BASIC_SET,
 )
 from pyzwave.message import Message
 from pyzwave.types import uint8_t
@@ -22,5 +23,14 @@ class Report(Message):
     """Command Class message COMMAND_CLASS_BASIC BASIC_REPORT"""
 
     NAME = "REPORT"
+
+    attributes = (("value", uint8_t),)
+
+
+@ZWaveMessage(COMMAND_CLASS_BASIC, BASIC_SET)
+class Set(Message):
+    """Command Class message COMMAND_CLASS_BASIC BASIC_SET"""
+
+    NAME = "SET"
 
     attributes = (("value", uint8_t),)
