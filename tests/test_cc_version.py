818b20c12f5cb38f497d6ccd763e00c5047e9880
Micke Prag <micke.prag@telldus.se>
2020-01-31 15:36:32 +0100

    Remove duplicate code

--- tests/test_cc_version.py
+++ tests/test_cc_version.py
@@ -5,27 +5,7 @@ import pytest
 
 from pyzwave.commandclass import Version
 
-from test_commandclass import MockNode
-
-
-@pytest.fixture
-def version() -> Version.Version:
-    node = MockNode([Version.COMMAND_CLASS_VERSION])
-    node.queue(
-        Version.VersionCommandClassReport(
-            requestedCommandClass=Version.COMMAND_CLASS_VERSION, commandClassVersion=4,
-        )
-    )
-    node.queue(
-        Version.VersionReport(
-            zwaveLibraryType=6,
-            zwaveProtocolVersion=1,
-            zwaveProtocolSubVersion=2,
-            applicationVersion=1,
-            applicationSubVersion=0,
-        )
-    )
-    return node.supported[Version.COMMAND_CLASS_VERSION]
+from test_commandclass import MockNode, version
 
 
 @pytest.mark.asyncio
