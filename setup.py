99bae10af2642c6c2561f9d11b2f55db121f2ef0
Micke Prag <micke.prag@telldus.se>
2020-02-21 10:06:06 +0100

    Update setup.py metadata

--- setup.py
+++ setup.py
@@ -3,13 +3,27 @@
 from setuptools import setup
 import pyzwave
 
+with open("README.md", "r") as fh:
+    longDescription = fh.read()  # pylint: disable=invalid-name
+
 setup(
     name="pyzwave",
     version=pyzwave.__version__,
     description="Library implementing a Z-Wave stack",
+    long_description=longDescription,
+    long_description_content_type="text/markdown",
     url="http://github.com/pyzwave/pyzwave",
     author="Micke Prag",
     author_email="micke.prag@telldus.se",
     license="GPL-3.0",
     packages=["pyzwave"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Home Automation",
+    ],
+    python_requires=">=3.6",
 )
