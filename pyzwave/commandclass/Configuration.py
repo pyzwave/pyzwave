80ee1d7f21929edcda2882baf823ee987e5a31c2
Micke Prag <micke.prag@telldus.se>
2020-04-08 15:18:04 +0200

    Implement caching of configuration values

--- pyzwave/commandclass/Configuration.py
+++ pyzwave/commandclass/Configuration.py
@@ -3,21 +3,26 @@ import logging
 
 from pyzwave.const.ZW_classcmd import (
     COMMAND_CLASS_CONFIGURATION,
+    CONFIGURATION_BULK_GET_V2,
+    CONFIGURATION_BULK_REPORT_V2,
     CONFIGURATION_GET,
     CONFIGURATION_REPORT,
     CONFIGURATION_SET,
 )
 from pyzwave.message import Message
 from pyzwave.types import (
+    BitStreamReader,
     BitStreamWriter,
+    bytes_t,
     enum_t,
     flag_t,
     reserved_t,
     uint3_t,
     uint8_t,
+    uint16_t,
 )
-from . import ZWaveCommandClass, ZWaveMessage
-from .CommandClass import CommandClass
+from . import ZWaveCommandClass, ZWaveMessage, ZWaveMessageHandler
+from .CommandClass import CommandClass, DictAttribute, VarDictAttribute
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -33,12 +38,37 @@ class Size(IntEnum):
 Size_t = enum_t(Size, uint3_t)  # pylint: disable=invalid-name
 
 
+@ZWaveMessage(COMMAND_CLASS_CONFIGURATION, CONFIGURATION_BULK_GET_V2)
+class BulkGet(Message):
+    """Command Class message COMMAND_CLASS_CONFIGURATION CONFIGURATION_BULK_GET_V2"""
+
+    NAME = "BULK_GET"
+    attributes = (("parameterOffset", uint16_t), ("numberOfParameters", uint8_t))
+
+
+@ZWaveMessage(COMMAND_CLASS_CONFIGURATION, CONFIGURATION_BULK_REPORT_V2)
+class BulkReport(Message):
+    """Command Class message COMMAND_CLASS_CONFIGURATION CONFIGURATION_BULK_REPORT_V2"""
+
+    NAME = "BULK_REPORT"
+    attributes = (
+        ("parameterOffset", uint16_t),
+        ("numberOfParameters", uint8_t),
+        ("reportsToFollow", uint8_t),
+        ("default", flag_t),
+        ("handshake", flag_t),
+        ("-", reserved_t(3)),
+        ("size", Size_t),
+        ("parameter", bytes_t),
+    )
+
+
 @ZWaveMessage(COMMAND_CLASS_CONFIGURATION, CONFIGURATION_GET)
 class Get(Message):
     """Command Class message COMMAND_CLASS_CONFIGURATION CONFIGURATION_GET"""
 
     NAME = "GET"
-    attributes = ("parameterNumber", uint8_t)
+    attributes = (("parameterNumber", uint8_t),)
 
 
 @ZWaveMessage(COMMAND_CLASS_CONFIGURATION, CONFIGURATION_REPORT)
@@ -47,7 +77,16 @@ class Report(Message):
 
     NAME = "REPORT"
 
-    # attributes = (("batteryLevel", uint8_t),)
+    attributes = (
+        ("parameterNumber", uint8_t),
+        ("-", reserved_t(5)),
+        ("size", Size_t),
+        ("value", int),
+    )
+
+    def parse_value(self, stream: BitStreamReader):  # pylint: disable=invalid-name
+        """Decode the value from the report"""
+        return int.from_bytes(stream.value(self.size), "big", signed=False)
 
 
 @ZWaveMessage(COMMAND_CLASS_CONFIGURATION, CONFIGURATION_SET)
@@ -69,9 +108,38 @@ class Set(Message):
         stream.addBytes(self.value, self.size, False)
 
 
+class ConfigurationValue(DictAttribute):
+    """Helper class for holding one configuration value"""
+
+    attributes = (("value", int), ("size", int))
+
+
 # pylint: disable=attribute-defined-outside-init
 @ZWaveCommandClass(COMMAND_CLASS_CONFIGURATION)
 class Configuration(CommandClass):
     """Command Class COMMAND_CLASS_CONFIGURATION"""
 
     NAME = "CONFIGURATION"
+    attributes = (("parameters", VarDictAttribute(int, ConfigurationValue)),)
+
+    @ZWaveMessageHandler(Report)
+    async def __report__(self, report: Report):
+        number = int(report.parameterNumber)
+        parameter = self.parameters.setdefault(number, ConfigurationValue())
+        parameter.value = report.value
+        parameter.size = int(report.size)
+        self.speak("commandClassUpdated")
+        return True
+
+    async def get(self, number: int, cached: bool = True) -> int:
+        """Request configuration value from node. Return the cached value if it is already known."""
+        if cached and number in self.parameters:
+            return self.parameters[number].value
+        report: Report = await self.sendAndReceive(Get(parameterNumber=number), Report)
+        return report.value
+
+    async def set(self, parameterNumber: int, size: Size, value: int) -> bool:
+        """Set a configuration value in the node"""
+        return await self.send(
+            Set(parameterNumber=parameterNumber, default=False, size=size, value=value,)
+        )
--- tests/test_cc_configuration.py
+++ tests/test_cc_configuration.py
@@ -1,10 +1,69 @@
 # pylint: disable=missing-function-docstring
 # pylint: disable=invalid-name
+# pylint: disable=redefined-outer-name
+
+import pytest
 
 from pyzwave.commandclass import Configuration
+from pyzwave.message import Message
+
+from test_commandclass import MockNode
 
 
-def test_set():
+@pytest.fixture
+def configuration() -> Configuration.Configuration:
+    node = MockNode([Configuration.COMMAND_CLASS_CONFIGURATION])
+    node.queue(Configuration.Report(value=42))
+    return node.supported[Configuration.COMMAND_CLASS_CONFIGURATION]
+
+
+@pytest.mark.asyncio
+async def test_Configuration_get_cached(configuration: Configuration.Configuration):
+    configuration.parameters[1] = Configuration.ConfigurationValue(value=42)
+    assert await configuration.get(1) == 42
+    configuration.node.assert_message_not_sent(Configuration.Get)
+
+
+@pytest.mark.asyncio
+async def test_Configuration_get_force_non_cached(
+    configuration: Configuration.Configuration,
+):
+    configuration.parameters[1] = Configuration.ConfigurationValue(value=42)
+    assert await configuration.get(1, cached=False) == 42
+    configuration.node.assert_message_sent(Configuration.Get)
+
+
+@pytest.mark.asyncio
+async def test_Configuration_get_non_cached(configuration: Configuration.Configuration):
+    assert await configuration.get(1) == 42
+    configuration.node.assert_message_sent(Configuration.Get)
+
+
+@pytest.mark.asyncio
+async def test_Configuration_report(configuration: Configuration.Configuration):
+    await configuration.node.handleMessage(
+        Configuration.Report(parameterNumber=2, size=2, value=1234)
+    )
+    assert configuration.parameters[2].size == 2
+    assert configuration.parameters[2].value == 1234
+
+
+@pytest.mark.asyncio
+async def test_Configuration_set(configuration: Configuration.Configuration):
+    await configuration.set(1, 1, 42)
+    configuration.node.assert_message_sent(Configuration.Set)
+
+
+def test_Report():
+    pkt = b"\x70\x06\x01\x01\x09"
+    msg = Message.decode(pkt)
+    assert isinstance(msg, Configuration.Report)
+    assert msg.parameterNumber == 1
+    assert msg.size == 1
+    assert msg.value == 9
+
+
+def test_Set():
     msg = Configuration.Set(parameterNumber=1, default=False, size=1, value=0x42)
     assert msg.compose() == b"\x70\x04\x01\x01\x42"
     msg = Configuration.Set(parameterNumber=1, default=False, size=2, value=0x1234)
